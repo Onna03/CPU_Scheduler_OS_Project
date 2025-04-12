6569b0de3b15fd7eaa628b620e035d1a37bbc53c
Bassant Samy <167112462+BassantSamy23@users.noreply.github.com>
2025-04-12 23:35:02 +0200

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -2,6 +2,7 @@ import java.util.List;
 
 public interface Scheduler {
     void schedule(List<Process> processes);
+    Process scheduleStep(List<Process> processesCopy, int currentTime, List<GanttEntry> liveGantt); // Updated to accept int
     List<GanttEntry> getGanttChart();
 
 }
