549f1343a34b89a2748ef877c5d3a0487ae1c34b
Onna03 <163770284+Onna03@users.noreply.github.com>
2025-04-16 14:38:07 +0200

    Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -2,6 +2,9 @@ import java.util.List;
 
 public interface Scheduler {
     void schedule(List<Process> processes);
+    Process scheduleStep(List<Process> processesCopy, int currentTime, List<GanttEntry> liveGantt); // Updated to accept int
     List<GanttEntry> getGanttChart();
 
+    // Add reset method to the interface
+    void reset();
 }
